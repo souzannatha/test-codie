250505c233df1e23c68bed2662d86306c1fd928f
souzannatha <souzanatha04@gmail.com>
2024-10-22 19:13:56 -0300

    feat: first commit.

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Codie - Centro Pokémon",
+  description: "Teste Técnico para a empresa Codie",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
