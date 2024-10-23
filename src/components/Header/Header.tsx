54a57a68a43ed674908bd6e246fbb52154967d18
souzannatha <souzanatha04@gmail.com>
2024-10-23 16:15:25 -0300

    feat: add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { Logo, TagHeader, TagAnchor, TagButton } from "./styles";
+import Image from "next/image";
+import pokeballIcon from "../../../public/images/white-pokeball.svg";
+
+export default function Header() {
+  return (
+    <>
+      <TagHeader>
+        <Logo>
+          <Image
+            src={pokeballIcon}
+            alt="Logotipo Centro Pokémon"
+            width={37}
+            height={34}
+            priority
+          />
+          Centro Pokémon
+        </Logo>
+        <nav>
+          <TagAnchor href="#about">Quem somos</TagAnchor>
+          <TagButton>Agendar Consulta</TagButton>
+        </nav>
+      </TagHeader>
+    </>
+  );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const TagHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 80px;
+`;
+
+export const Logo = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 260px;
+  height: 61px;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 50px;
+  background-color: #e40f0f;
+  font-size: 0.875rem;
+  color: #ffffff;
+  font-weight: bold;
+  gap: 1.25rem;
+  cursor: pointer;
+`;
+
+export const TagButton = styled.button`
+  width: 172px;
+  height: 42px;
+  padding: 12px 28px;
+  border: none;
+  border-radius: 30px;
+  background-color: #e40f0f;
+  font-size: 14;
+  color: #ffffff;
+  font-weight: bold;
+  cursor: pointer;
+  margin-left: 1.875rem;
+`;
+
+export const TagAnchor = styled.a`
+  font-size: 0.875rem;
+  font-weight: 400;
+  color: #000000;
+  cursor: pointer;
+`;
