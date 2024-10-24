08e1cf9edf8df9fe03119b90a1a01adbba65bf4a
souzannatha <souzanatha04@gmail.com>
2024-10-24 05:38:35 -0300

    feat: add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { TagFooter } from "./styles";
+import React from "react";
+
+export function Footer() {
+  return (
+    <TagFooter>
+      <p>
+        Todas as marcas e ilustrações utilizadas são de seus resepctivos donos.
+      </p>
+    </TagFooter>
+  );
+}
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const TagFooter = styled.footer`
+  width: 100vw;
+  padding: 24px 0px;
+  background-color: #1d1d1d;
+
+  p {
+    text-align: center;
+    color: #ffffff;
+    font-weight: 400;
+    font-size: 14px;
+  }
+`;
