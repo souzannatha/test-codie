1eb878b2b51ccd2dfd773dde5bb065b5a5a2f641
souzannatha <souzanatha04@gmail.com>
2024-10-24 05:34:51 -0300

    feat: add backgroundSection component

---
+++ src/components/BackgroundSection/BackgroundSection.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { TagSection } from "./styles";
+
+export function BackgroundSection() {
+  return (
+    <TagSection>
+      <h1>Cuidamos bem do seu pokémon, para ele cuidar bem de você</h1>
+    </TagSection>
+  );
+}
---
+++ src/components/BackgroundSection/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const TagSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-image: url("/images/pokemon-hero.jpg");
+  background-size: cover;
+  background-position: center;
+  width: 100vw;
+  height: 45rem;
+
+  h1 {
+    max-width: 510px;
+    color: #fff;
+    font-size: 2rem;
+    font-weight: bold;
+  }
+`;
